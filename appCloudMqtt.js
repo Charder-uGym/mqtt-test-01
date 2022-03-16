81ab22437b7da585b3ab624d9f5914095cfca8f6
paulchainkang <paul@paul-kang.com>
2022-03-16 08:59:40 +0800

    wip

--- appCloudMqtt.js
+++ appCloudMqtt.js
@@ -1,32 +1,6 @@
-//var mqtt = require('mqtt'), url = require('url');
-//// Parse
-////var mqtt_url = url.parse(process.env.CLOUDMQTT_URL || 'mqtt://localhost:1883');
-//var mqtt_url = url.parse('mqtt://nbitjbzu:DxYegPp9xHAY@driver.cloudmqtt.com:18578');
-//var auth = (mqtt_url.auth || ':').split(':');
-//console.log(auth);
-//
-//// Create a client connection
-//var client = mqtt.createClient(mqtt_url.port, mqtt_url.hostname, {
-//  username: auth[0],
-//  password: auth[1]
-//});
-//
-//client.on('connect', function() { // When connected
-//
-//  // subscribe to a topic
-//  client.subscribe('hello/world', function() {
-//    // when a message arrives, do something with it
-//    client.on('message', function(topic, message, packet) {
-//      console.log("Received '" + message + "' on '" + topic + "'");
-//    });
-//  });
-//
-//  // publish a message to a topic
-//  client.publish('hello/world', 'my message', function() {
-//    console.log("Message is published");
-//    client.end(); // Close the connection when published
-//  });
-//});
+const express = require('express');
+var app = express();
+var port = process.env.PORT || 5000
 
 const mqtt = require('mqtt'); //"mqtt": "^0.3.13"
 const client  = mqtt.connect('mqtt://nbitjbzu:DxYegPp9xHAY@driver.cloudmqtt.com:18578')
@@ -45,3 +19,13 @@ client.on('message', function (topic, message) {
   console.log(message.toString())
   //client.end()
 })
+
+app.get('/', async function (req, res) {
+  res.send("Hello");
+});
+
+app.listen(port, function () {
+  console.log('App listening on port: ', port);
+});
+        
+        
